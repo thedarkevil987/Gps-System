44cea0bdb561e4cfdb2cd25b26042155e4c980bd
Yousef Shawky <112232675+thedarkevil987@users.noreply.github.com>
2023-05-12 16:51:27 +0300

    Add files via upload

---
+++ UART/Uart.h
@@ -0,0 +1,12 @@
+
+
+
+void UART1_Init(void);
+
+char UART1_ReadChar(void);
+
+void UART1_WriteChar(char data);
+
+void UART1_WriteString(char *str);
+
+void UART1_ReadString(char *str, char stopCh);
