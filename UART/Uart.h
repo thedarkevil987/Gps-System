361ead85e54438446c5f4937ebe6834856729ef9
MohamedAlii59 <126531693+MohamedAlii59@users.noreply.github.com>
2023-05-13 19:11:48 +0300

    Update Uart.h

--- UART/Uart.h
+++ UART/Uart.h
@@ -1,1 +1,14 @@
 
+void UART1_ReadString(char *str, char stopCh)
+{
+	char c = UART1_ReadChar();
+
+	while(c && c != stopCh)
+    {
+		*str = c;
+		str++;
+		c = UART1_ReadChar();
+	}
+	*str = 0x00;
+
+}
