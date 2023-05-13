f5420dee8042bd879526597634259a4ebc21140b
Yousef Shawky <112232675+thedarkevil987@users.noreply.github.com>
2023-05-13 18:28:57 +0300

    Update Gps.c

--- GPS/Gps.c
+++ GPS/Gps.c
@@ -1,1 +1,50 @@
+#include "tm4c123gh6pm.h"
+#include "GPIO.h"
+#include "UART.h"
+#include "GPS.h"
+#include "stdint.h"
+#include "math.h"
+#include "stdlib.h"
+#include "stdio.h"
 
+
+#define PI 3.14159265359
+
+
+
+extern char a[9];
+extern char f[9][17];
+
+
+
+
+void get_Degree_From_Latitude(char e,char w)
+{
+
+ a[0]=e;
+ a[1]=w;
+ a[2]='\0';
+
+}
+void get_Degree_From_Longitude(char e,char w,char r)
+{
+
+ a[0]=e;
+ a[1]=w;
+ a[2]=r;
+ a[3]='\0';
+
+}
+
+void get_mintues(char i, char j)
+{
+
+ char end = j+8;
+ char k;
+
+ for(k =0; k< end; k++)
+ {
+   a[k] = f[i][j++];
+ }
+
+}
