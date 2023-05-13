d8b9b18e81a9f52d5215608593f087d0d55c65a9
philo1698 <131830174+philo1698@users.noreply.github.com>
2023-05-13 21:21:52 +0300

    Update Gps.h

--- GPS/Gps.h
+++ GPS/Gps.h
@@ -1,1 +1,13 @@
 
+void get_Degree_From_Latitude(char e,char w);
+
+void get_Degree_From_Longitude(char e,char w,char r);
+
+void get_mintues(char i, char j);
+
+float latitude_To_Decimal_Degree(void);
+
+float longitude_To_Decimal_Degree(void);
+
+float getDistance(float s_lat,float s_long,float end_lat,float end_long);
+
