c0339b6bd1e8bf5eeccfecffb9743a4360c23fa7
YoussefGobran <132088403+YoussefGobran@users.noreply.github.com>
2023-05-13 21:14:18 +0300

    Update Gpio.h

--- GPIO/Gpio.h
+++ GPIO/Gpio.h
@@ -1,1 +1,15 @@
+#define RED 0x02
+#define BLUE 0x04
+#define GREEN 0x08
+#define YELLOW 0x0A
+#define CR 0x0D
+
+
+void Led_Init(void);
+
+void Output(unsigned char color);
+
+void RGB_setOutput(char data);
+
+void LEDS_OFF(void);
 
