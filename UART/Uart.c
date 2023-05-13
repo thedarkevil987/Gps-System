96fd31e5c34e414a5e1c573000f65765bf277f5a
Ezzat Abdelrazek <111779523+Ezzat-Abdelrazek@users.noreply.github.com>
2023-05-13 18:55:20 +0300

    Update Uart.c

--- UART/Uart.c
+++ UART/Uart.c
@@ -1,1 +1,30 @@
 
+char UART1_ReadChar()
+{
+
+ while((UART1_FR_R&0x10)!=0){}
+
+ return (char)UART1_DR_R &0xFF;
+
+}
+
+
+void UART1_WriteChar(char data)
+{
+
+ while((UART1_FR_R&0x20)!=0){}
+
+ UART1_DR_R=data;
+
+}
+
+void UART1_WriteString(char *str)
+{
+
+  while(*str)
+  {
+    UART1_WriteChar(*str);
+    str++;
+  }
+
+}
